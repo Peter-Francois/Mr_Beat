987ff8727bfcc63c88854ade8f27c01ab917c97a
Peter Francois <peter.francois237@hotmail.com>
2025-04-10 15:13:14 +0200

    feat(mute): def mute function

--- track.py
+++ track.py
@@ -38,26 +38,27 @@ class TrackSoloButton(ToggleButton):
 # Creation of track buttons and their corresponding step buttons.
 class TrackWidget(BoxLayout):
     def __init__(self, sound, audio_engine, tracks_nb_steps, track_source, steps_left_align,
-                 tracks_widget_size_hint_min_y, tracks_widget_size_hint_max_y, **kwargs):
+                 tracks_widget_size_hint_min_y, tracks_widget_size_hint_max_y, nb_tracks, **kwargs):
         super(TrackWidget, self).__init__(**kwargs)
         self.audio_engine = audio_engine
         self.size_hint_max_y = tracks_widget_size_hint_max_y
         self.size_hint_min_y = tracks_widget_size_hint_min_y
         self.sound = sound 
         self.track_source = track_source
+        self.nb_tracks = nb_tracks
         box_layout_actions_button = BoxLayout()
         box_layout_actions_button.size_hint_x = None
         # Retrieval of the offset for the step buttons and the separator to adjust the position of the play_indicator.
         box_layout_actions_button.width = steps_left_align
-        sound_button = TrackSoundButton()
-        sound_button.size_hint_x = None
-        sound_button.width = box_layout_actions_button.width * .5
+        self.sound_button = TrackSoundButton()
+        self.sound_button.size_hint_x = None
+        self.sound_button.width = box_layout_actions_button.width * .5
         # Retrieving the sound name from main.py (sound).
-        sound_button.text = sound.displayname
-        sound_button.on_press = self.on_sound_button_press
-        sound_button.background_normal = "images/sound_button_normal.png"
-        sound_button.background_down = "images/sound_button_down.png"
-        box_layout_actions_button.add_widget(sound_button)
+        self.sound_button.text = sound.displayname
+        self.sound_button.on_press = self.on_sound_button_press
+        self.sound_button.background_normal = "images/sound_button_normal.png"
+        self.sound_button.background_down = "images/sound_button_down.png"
+        box_layout_actions_button.add_widget(self.sound_button)
 
         # box layout for volume control
         box_layout_volume_control = BoxLayout()
@@ -110,21 +111,16 @@ class TrackWidget(BoxLayout):
         clear_button = TrackClearButton()
         clear_button.on_press = self.on_clear_button_press
         box_layout_clear_mute_solo_button.add_widget(clear_button)
-        mute_button = TrackMuteButton()
-        mute_button.on_press = self.on_mute_button_pressed
-        box_layout_clear_mute_solo_button.add_widget(mute_button)
-        clear_button = TrackClearButton()
-        clear_button.on_press = self.on_clear_button_press
-        box_layout_clear_mute_solo_button.add_widget(clear_button)
+        # mute button
+        self.mute_button = TrackMuteButton()
+        self.mute_button.on_press = self.on_mute_button_pressed
+        box_layout_clear_mute_solo_button.add_widget(self.mute_button)
 
+        '''# solo button
+        self.solo_button = TrackSoloButton()
+        self.solo_button.on_press = self.on_solo_button_pressed
+        box_layout_clear_mute_solo_button.add_widget(self.solo_button)'''
 
-        """ solo button
-        solo_button = TrackSoloButton()
-        solo_button.background_normal = "images/track_separator.png"
-        solo_button.background_down = "images/track_separator.png"
-        solo_button.on_press = self.on_solo_button_press
-        box_layout_solo_mute.add_widget(solo_button)
-        """
         box_layout_actions_button.add_widget(box_layout_clear_mute_solo_button)
         self.add_widget(box_layout_actions_button)
 
@@ -166,6 +162,14 @@ class TrackWidget(BoxLayout):
         else:
             self.volume_slider.value = 0
             self.mute_button.color = [0, 0, 0, 1]
+    
+    '''def on_solo_button_pressed(self):
+        for i in range(0, self.nb_tracks):
+            if self.sound_button.text != "down":
+                pass
+            else:
+                self.mute_button.state = "down"
+                self.on_mute_button_pressed()'''
 
 
     def on_step_button_state(self, widget, value):
